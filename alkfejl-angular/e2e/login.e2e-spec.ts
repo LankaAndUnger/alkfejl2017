1356716e839d946fff487e19c3720b573819b417
Unger Alex <aunger@loginet.hu>
2017-12-14 11:15:36 +0100

    add first test

--- alkfejl-angular/e2e/login.e2e-spec.ts
+++ alkfejl-angular/e2e/login.e2e-spec.ts
@@ -12,21 +12,21 @@ describe('Login functionality', () => {
   });
 
   it('should fail to login for empty credentials', () => {
-    element(by.buttonText('Bejelentkezés')).click();
+    element(by.css('#Bejelentkezés')).click();
     expect(getPath()).toEqual('/login');
   });
 
   it('should fail to login for invalid credentials', () => {
-    element(by.css('input[type="email"]')).sendKeys('éasklduélasukdtzél@léasdkélasku.aléskdj');
+    element(by.css('input[type="text"]')).sendKeys('éasklduélasukdtzél@léasdkélasku.aléskdj');
     element(by.css('input[type="password"]')).sendKeys('éasklduélasukdtzé');
-    element(by.buttonText('Bejelentkezés')).click();
+    element(by.css('#Bejelentkezés')).click();
     expect(getPath()).toEqual('/login');
   });
 
   it('should log in for valid credentials', () => {
-    element(by.css('input[type="username"]')).sendKeys('asd');
+    element(by.css('input[type="text"]')).sendKeys('asd');
     element(by.css('input[type="password"]')).sendKeys('asd123');
-    element(by.buttonText('Bejelentkezés')).click();
-    expect(getPath()).toEqual('/vehicles');
+    element(by.css('#Bejelentkezés')).click();
+    expect(getPath()).toEqual('/vehicle');
   });
 });
