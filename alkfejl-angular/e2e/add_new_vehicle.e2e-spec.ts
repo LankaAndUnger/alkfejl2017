9ae054693b685998cd84ddc7576fefdbc73e65c2
Lanka Máté <matthewld@INF.ELTE.HU>
2017-12-14 11:34:56 +0100

    added new tests for vehicle add

---
+++ alkfejl-angular/e2e/add_new_vehicle.e2e-spec.ts
@@ -0,0 +1,85 @@
+import { browser, by, element } from 'protractor';
+import { getPath } from './getpath';
+
+describe('Login functionality', () => {
+  beforeEach(() => {
+    browser.get('/addVehicle');
+  });
+
+  it('should navigate to the login page', () => {
+    // Elvárt eredmény
+    expect(getPath()).toEqual('/addVehicle');
+  });
+
+  it('should fail for empty values', () => {
+    element(by.buttonText('Küldés')).click();
+    expect(getPath()).toEqual('/addVehicle');
+  });
+
+  it('should fail for some empty values', () => {
+    element(by.css('#plate')).sendKeys('XXD-123');
+    element(by.css('#accent')).sendKeys('Civic');
+    element(by.buttonText('Küldés')).click();
+    expect(getPath()).toEqual('/addVehicle');
+  });
+
+  it('should fail for negative vintage', () => {
+    element(by.css('#plate')).sendKeys('XXD-123');
+    element(by.css('#accent')).sendKeys('Civic');
+    element(by.css('#brand')).sendKeys('Honda');
+    element(by.css('#vintage')).sendKeys('-1');
+    element(by.css('#price')).sendKeys('5000');
+    element(by.buttonText('Küldés')).click();
+    expect(getPath()).toEqual('/addVehicle');
+  });
+
+  it('should fail for later vintage than current', () => {
+    element(by.css('#plate')).sendKeys('XXD-123');
+    element(by.css('#accent')).sendKeys('Civic');
+    element(by.css('#brand')).sendKeys('Honda');
+    element(by.css('#vintage')).sendKeys('2500');
+    element(by.css('#price')).sendKeys('5000');
+    element(by.buttonText('Küldés')).click();
+    expect(getPath()).toEqual('/addVehicle');
+  });
+
+  it('should fail for too short vintage', () => {
+    element(by.css('#plate')).sendKeys('XXD-123');
+    element(by.css('#accent')).sendKeys('Civic');
+    element(by.css('#brand')).sendKeys('Honda');
+    element(by.css('#vintage')).sendKeys('500');
+    element(by.css('#price')).sendKeys('5000');
+    element(by.buttonText('Küldés')).click();
+    expect(getPath()).toEqual('/addVehicle');
+  });
+
+  it('should fail for too long vintage', () => {
+    element(by.css('#plate')).sendKeys('XXD-123');
+    element(by.css('#accent')).sendKeys('Civic');
+    element(by.css('#brand')).sendKeys('Honda');
+    element(by.css('#vintage')).sendKeys('50000');
+    element(by.css('#price')).sendKeys('5000');
+    element(by.buttonText('Küldés')).click();
+    expect(getPath()).toEqual('/addVehicle');
+  });
+
+  it('should fail for negative price', () => {
+    element(by.css('#plate')).sendKeys('XXD-123');
+    element(by.css('#accent')).sendKeys('Civic');
+    element(by.css('#brand')).sendKeys('Honda');
+    element(by.css('#vintage')).sendKeys('1999');
+    element(by.css('#price')).sendKeys('-1');
+    element(by.buttonText('Küldés')).click();
+    expect(getPath()).toEqual('/addVehicle');
+  });
+
+  it('should pass for correct values', () => {
+    element(by.css('#plate')).sendKeys('XXD-123');
+    element(by.css('#accent')).sendKeys('Civic');
+    element(by.css('#brand')).sendKeys('Honda');
+    element(by.css('#vintage')).sendKeys('1999');
+    element(by.css('#price')).sendKeys('5000');
+    element(by.buttonText('Küldés')).click();
+    expect(getPath()).toEqual('/vehicle');
+  });
+});
